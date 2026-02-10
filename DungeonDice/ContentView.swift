7664c7e9712ce563e91315d13da40861f21529ab
Beatriz <btovar0002@mymail.lausd.net>
2026-02-10 10:22:08 -0800

    2nd commit

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -41,8 +41,8 @@ struct ContentView: View {
                 .frame(height: 150)
             
             Spacer()
-            
 
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
                 ForEach(Dice.allCases, id: \.self) { dice in
                     Button("\(dice.rawValue)-sided") {
                         resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
@@ -50,6 +50,8 @@ struct ContentView: View {
                 }
             .buttonStyle(.borderedProminent)
             .tint(.red)
+            }
+        
 
         }
         .padding()
